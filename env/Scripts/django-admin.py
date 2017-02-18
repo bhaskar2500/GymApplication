c6b3dc5d1b8a36e225b2aa3fa3b8754596221270
bhaskar2500 <bhaskar.2500@gmail.com>
2017-02-18 19:10:37 +0530

    'change'

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Bhaskar Kaushal\Desktop\GymProject-master\CreateAPI\env\Scripts\pythonw.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
